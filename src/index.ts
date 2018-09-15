1f0d7b89152b9e8347fb3881619b5653293606ec
vcc1432 <valerie.gersen@gmail.com>
2018-09-15 11:09:19 +0200

    API setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+   controllers: []
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
